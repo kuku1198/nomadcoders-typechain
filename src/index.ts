d5ee0e32eb4974d57f813eb4212c488bfc8956d9
어영규 <kuku1198@gmail.com>
2021-09-19 14:37:16 +0900

    #0.7

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+class Human {
+    public name: string;
+    private age: number;
+    public gender: string;
+
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const parson = {
-    name: "YeongGyu",
-    age: 31,
-    gender: "male",
-};
+const yeongGyu = new Human("YeongGyu", 31, "male")
 
 const sayHi = (parson: Human): string => {
     return `Hello ${parson.name}, you are ${parson.age}, you are a ${parson.gender}`;
 }
 
-console.log(sayHi(parson));
+console.log(sayHi(yeongGyu));
 
 export {};
