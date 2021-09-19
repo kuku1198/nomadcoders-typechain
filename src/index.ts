fe8a90608a4453418cef62b796e1697521c464be
어영규 <kuku1198@gmail.com>
2021-09-19 14:53:33 +0900

    #0.8

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
-class Human {
-    public name: string;
-    private age: number;
-    public gender: string;
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
 
-    constructor(name: string, age: number, gender: string) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number,
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const yeongGyu = new Human("YeongGyu", 31, "male")
+const genesisBlock: Block = new Block(0, "123213123", "", "Hello", 123456);
 
-const sayHi = (parson: Human): string => {
-    return `Hello ${parson.name}, you are ${parson.age}, you are a ${parson.gender}`;
-}
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHi(yeongGyu));
+console.log(blockchain);
 
 export {};
